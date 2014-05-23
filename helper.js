20a6e1bfaf34693dc73278314c24744c628218c8
Tim Plummer <plummert@amazon.com>
2014-05-23 09:55:47 -0700

    "cleanup,_moved_stuff_to_separate_file._Committed_from_my_Node.js_Shell™."

---
+++ helper.js
@@ -0,0 +1,56 @@
+var os = require('os');
+var child_process = require('child_process');
+
+var errorMap = {
+    'ENOENT' : "Command not found.",
+};
+var customCommands = {
+    'cd' : function(path) { process.chdir(path); },
+    'pwd' : function() { console.log(process.cwd()); },
+};
+
+function logError(msg) {
+    return function(err) {
+        if(errorMap[err.code]) {
+            console.log(msg + ': ' + errorMap[err.code]);
+        } else {
+            console.log('Error: ' + err.code);
+        }
+    }
+}
+
+var compose = exports.compose = function(fn, callback) {
+    return function() {
+        fn();
+        callback();
+    }
+}
+
+function commandEnd() {
+    process.stdin.unpipe();
+}
+
+exports.getUsername = function() {
+    if(os.platform() === 'win32') {
+        return process.env['USERNAME'];
+    } else {
+        return process.env['USER'];
+    }
+};
+
+exports.runCommand = function(line, callback) {
+  callback = compose(commandEnd, callback);
+  var args = line.trim().split(' ');
+  var cmd = args.shift();
+  if(customCommands[cmd]) {
+    customCommands[cmd].apply(null, args);
+    callback();
+  } else {
+    var child = child_process.spawn(cmd, args);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('exit', callback);
+    child.on('error', compose(logError(cmd), callback));
+  }
+};
--- nosh.js
+++ nosh.js
@@ -1,6 +1,6 @@
 #!/bin/node
 
-var os = require('os');
+var helper = require('./helper.js');
 var readline = require('readline');
 var child_process = require('child_process');
 
@@ -18,16 +18,8 @@ var keys = process.stdin.on('data', function(key) {
     }
 });
 
-function getUsername() {
-    if(os.platform() === 'win32') {
-        return process.env['USERNAME'];
-    } else {
-        return process.env['USER'];
-    }
-};
-
 function setPrompt() {
-    var user = getUsername();process.env['USER'];
+    var user = helper.getUsername();
     var now = new Date();
     var ampm = now.getHours() <= 11 ? 'AM' : 'PM';
     var time = (now.getHours()%12) + ':'
@@ -38,44 +30,19 @@ function setPrompt() {
 
 var n = 0;
 function cont() {
+    rl.resume();
     n = n + 1;
     setPrompt();
     rl.prompt(true);
 }
-var errorMap = {
-    'ENOENT' : "Command not found: "
-};
-function logError(cmd, callback) {
-    return function(err) {
-        if(errorMap[err.code]) {
-            console.log(errorMap[err.code] + cmd);
-        } else {
-            console.log(err.code);
-        }
-        callback();
-    };
-}
 
-var customCommands = {
-    'cd' : function(path) { process.chdir(path); },
-    'pwd': function() { console.log(process.cwd()); },
-};
-/*rl.on('SIGINT', function() {
-    console.log('nope');
-});*/
+rl.on('SIGINT', function() {
+    console.log('cya');
+    process.exit(0);
+});
 rl.on('line', function(line) {
     if(line.length == 0) return;
-    var args = line.trim().split(' ');
-    var cmd = args.shift();
-    if(customCommands[cmd]) {
-        customCommands[cmd].apply(null, args);
-        cont();
-    } else {
-        var child = child_process.spawn(cmd, args);
-        child.stdout.pipe(process.stdout);
-        child.on('exit', cont);
-        child.on('error', logError(cmd, cont));
-    }
+    helper.runCommand(line, cont);
 });
 
 cont();
